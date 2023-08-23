776cd19f1d7d0c09069385805d598b34d0f4ae53
JuanGuzman1205 <jr20075jr@gmail.com>
2023-08-23 01:48:50 -0500

    Algoritmo con las 4 operaciones resuelto.

--- PrimerProyecto/PrimerProyecto/Program.cs
+++ PrimerProyecto/PrimerProyecto/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Enunciado:
+ * Ingresar por pantalla 2 números y calcular las 4 operaciones aritméticas con dichos números.
+ * Imprimir en pantalla el resultado de cada operacion*/
+
+//1ro: definir el nombre de mi clase
+public class MiPrimerPrograma
+{
+
+    static void Main(string[] args)
+    {
+        //2do. Declaracion de variables (Datos de entrada)
+        int num1, num2, suma, resta, producto, Cociente ; //Quemar codigo: poner valores directamente a las variables sin pedirlos desde la consola
+
+        //3ro. Pedir los valores por pantalla (consola)
+        Console.Write("Ingrese su primer número: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Ingrese su segundo número: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        //4to. Calculos (Procesos)
+        suma = num1 + num2;
+        resta = num1 - num2;
+        producto = num1 * num2;
+        Cociente = num1 / num2;
+
+        //5to. Imprimir los resultados (Datos de salida)
+        Console.Write("La suma es: " + suma );
+        Console.Write("\n");
+        Console.Write("La resta es: " + resta);
+        Console.Write("\n");
+        Console.Write("El producto es: " + producto);
+        Console.Write("\n");
+        Console.Write("El cociente es: " + Cociente);
+    }
+    
+
+    
+
+}
